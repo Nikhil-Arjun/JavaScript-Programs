506d800881e00f780b17f69d095b18782e9e9fb8
Nikhil-Arjun <nikhilarjun97@gmail.com>
2023-08-05 20:44:56 +0530

    positive-negative

---
+++ Set-2/positive-negative.js
@@ -0,0 +1,17 @@
+// Program to check if a number is positive, negative or zero
+
+// input from the user: number
+// const number = parseInt(prompt('Enter a number: '));
+
+const number = 0;
+
+// check if number is greater than 0
+if (number > 0) {
+  console.log('The number is positve');
+} //check if number is negative
+else if (number < 0) {
+  console.log('The number is negative');
+} // if number is less than 0
+else {
+  console.log('The number is zero');
+}
